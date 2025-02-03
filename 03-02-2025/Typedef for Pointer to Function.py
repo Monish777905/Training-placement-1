32fb1b769cc7160f5a007c65ce3620cccfdc8926
Monish777905 <monish9200@gmail.com>
2025-02-03 20:56:40 +0530

    Create Typedef for Pointer to Function.py

---
+++ 03-02-2025/Typedef for Pointer to Function.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+operation = add
+print(f"Sum: {operation(10, 5)}")
+
+operation = subtract
+print(f"Difference: {operation(10, 5)}")
